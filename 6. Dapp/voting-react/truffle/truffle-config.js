791ebe0d2e524ac0c7aaae02f83a26ff3971c9db
Quentin D.C <quent043@hotmail.com>
2022-07-15 15:49:33 +0200

    Updated Tesnets on truffle-config

--- 6. Dapp/voting-react/truffle/truffle-config.js
+++ 6. Dapp/voting-react/truffle/truffle-config.js
@@ -28,7 +28,6 @@ module.exports = {
       // from: <address>,        // Account to send transactions from (default: accounts[0])
       websocket: true         // Enable EventEmitter interface for web3 (default: false)
     },
-
     ropsten: {
       provider: () => new HDWalletProvider(
           {
@@ -36,21 +35,38 @@ module.exports = {
                 {
                   phrase:`${process.env.MNEMONIC}`
                 },
+            // providerOrUrl: `wss://mainnet.infura.io/ws/v3/${process.env.INFURA_ID}`
             providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`
           }),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      timeoutBlocks: 200000,
+      networkCheckTimeout: 2000000,// # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-
     kovan: {
-      provider: () => new HDWalletProvider({mnemonic: {phrase:`${process.env.MNEMONIC}`}, providerOrUrl: `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`}),
-      network_id: 2111,   // This network is yours, in the cloud.
+      provider: () => new HDWalletProvider({mnemonic: {phrase:`${process.env.MNEMONIC}`}, providerOrUrl: `https://kovan.infura.io/v3/${process.env.INFURA_ID}`}),
+      network_id: 42,   // This network is yours, in the cloud.
+      production: true,    // Treats this network as if it was a public net. (default: false)
+      gas: 5000000,
+      gasPrice: 25000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider({mnemonic: {phrase:`${process.env.MNEMONIC}`}, providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`}),
+      network_id: 4,   // This network is yours, in the cloud.
+      production: true,    // Treats this network as if it was a public net. (default: false)
+      timeoutBlocks: 200000,
+      networkCheckTimeout: 2000000,
+    },
+    goerli: {
+      provider: () => new HDWalletProvider({mnemonic: {phrase:`${process.env.MNEMONIC}`}, providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_ID}`}),
+      network_id: 5,   // This network is yours, in the cloud.
       production: true    // Treats this network as if it was a public net. (default: false)
     }
   },
+
+
 
   // Set default mocha options here, use special reporters, etc.
   plugins: ["solidity-coverage"],
